e3054d71d3988a5fbc79c0ece8e37e06ef9e6851
eguil <Eric.Guilyardi@locean-ipsl.upmc.fr>
2017-01-09 22:51:21 +0100

    Create metrics table in EnsoMetricsGraph.py

--- driveGraphs.py
+++ driveGraphs.py
@@ -1,8 +1,8 @@
 
 from EnsoMetricsGraph import EnsoMetricsTable
 
-#EnsoMetrics =[{'col1':'IPSL-CM5A-LR','col2':0.82,'col3':4.1},
-#              {'col1':'IPSL-CM5A-MR','col2':1.2,'col3':4.5}]
-EnsoMetrics =[[1,2,3],[4,5,6]]
+EnsoMetrics =[['IPSL-CM5A-LR','0.82','4.1'],
+              ['IPSL-CM5A-MR','1.2','4.5']]
+#EnsoMetrics =[[1,2,3],[4,5,6]]
 
 fig=EnsoMetricsTable(EnsoMetrics, 'EnsoMetrics')
