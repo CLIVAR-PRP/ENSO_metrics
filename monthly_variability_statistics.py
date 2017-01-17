6b31a1b8caaa494cd61711b773a6adebb065a571
eguil <Eric.Guilyardi@locean-ipsl.upmc.fr>
2017-01-17 14:39:40 +0100

    Use pcmdi tools for merge of ENSO metrics

--- monthly_variability_statistics.py
+++ monthly_variability_statistics.py
@@ -33,6 +33,7 @@ def get_slope_linear_regression_from_anomaly(y,x,sign_x):
       results = genutil.statistics.linearregression(y_area_avg_anom,x=x_area_avg_anom)
    elif sign_x == 1:
       idxplus = numpy.argwhere (x_area_avg_anom >= 0.)
+      print idxplus.shape
       results = genutil.statistics.linearregression(y_area_avg_anom[idxplus],x=x_area_avg_anom[idxplus])
    elif sign_x == -1:
       idxneg = numpy.argwhere (x_area_avg_anom <= 0.)
