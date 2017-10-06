f0e631ffc9e29ae5518b373c9fcb265523e7028d
Eric.Guilyardi@locean-ipsl.upmc.fr <ericglod@ciclad-ng.private.ipsl.fr>
2017-10-06 14:41:00 +0200

    Clean up following re-organisation

--- lib/__init_.py
+++ lib/__init_.py
@@ -1,3 +1,1 @@
-from EnsoMetricsLib import Ensoampln3, EnsoAmpl, EnsoMu, computeAnom  # noqa
-from metricsCollectionsLib import defCollection, metricReqs  # noqa
-from EnsoMetricsGraph import EnsoMetricsTable  # noqa
+
