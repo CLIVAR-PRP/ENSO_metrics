ebc20290c0637ba0ffbe2a6e9357c3a7bd96f7fc
lee1043 <lee1043@llnl.gov>
2017-10-10 16:47:31 -0700

    clean up

--- lib/version.py
+++ lib/version.py
@@ -1,3 +1,3 @@
 __version__ = '0.1'
 __git_tag_describe__ = '0.1'
-__git_sha1__ = 'bc095a1733a36feb16f59432ea41a349e8453c62'
+__git_sha1__ = '4cf7134926e010cf32b437cd4e102caafb74b182'
--- scripts/driver_enso_metrics.py
+++ scripts/driver_enso_metrics.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-import logging
-LOG_LEVEL = logging.INFO
-logging.basicConfig(level=LOG_LEVEL)
+#import logging
+#LOG_LEVEL = logging.INFO
+#logging.basicConfig(level=LOG_LEVEL)
 
 import cdms2
 import copy
@@ -15,7 +15,7 @@ import collections
 from collections import defaultdict
 
 from EnsoMetrics.EnsoCollectionsLib import *
-from EnsoMetrics.EnsoMetricsLib import EnsoAmpl, EnsoMu
+from EnsoMetrics.EnsoMetricsLib import *
 from EnsoMetrics.pmpParser import ReadOptions
 
 #=================================================
@@ -68,8 +68,7 @@ for mod in models:
     print sstFile
     print tauxFile
   
-    #try:
-    if 1:
+    try:
         for metric in metrics:
 
             print metric
@@ -78,15 +77,16 @@ for mod in models:
                 nBox = mcdict['dict_of_regions'][metric]['sst']
                 tmp_dict = EnsoAmpl(sstFile, sstName, nBox)
                 tmp_dict['input_data'] = [sstFile]
+                enso_stat_dic[mod][metric] = tmp_dict
+
             elif metric == 'EnsoMu':
                 sstBox = mcdict['dict_of_regions'][metric]['sst']
                 tauxBox = mcdict['dict_of_regions'][metric]['taux']
                 tmp_dict = EnsoMu(sstFile, tauxFile, sstName, tauxName, sstBox, tauxBox)
                 tmp_dict['input_data'] = [sstFile, tauxFile]
-        
-            enso_stat_dic[mod][metric] = tmp_dict
-    else:    
-    #except:
+                enso_stat_dic[mod][metric] = tmp_dict
+
+    except:
         print 'failed for ', mod
   
 #=================================================
@@ -104,9 +104,8 @@ disclaimer = open(
 metrics_dictionary = collections.OrderedDict()
 metrics_dictionary["DISCLAIMER"] = disclaimer
 metrics_dictionary["REFERENCE"] = "The statistics in this file are based on Bellenger, H et al. Clim Dyn (2014) 42:1999-2018. doi:10.1007/s00382-013-1783-z"
-metrics_dictionary["RESULTS"] = enso_stat_dic  # collections.OrderedDict()
+metrics_dictionary["RESULTS"] = enso_stat_dic
 
-#OUT.var = var
 OUT.write(
     metrics_dictionary,
     json_structure=["model", "metric", "item", "value or description"],
