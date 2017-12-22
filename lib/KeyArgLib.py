2b539127068804c63b1322b58e05eb18b3d412a7
yyplanton <yplanton@locean-ipsl.upmc.fr>
2017-12-22 14:56:33 +0100

    Reordered dictionaries in EnsoCollectionsLib.py, minor changes in other programs

---
+++ lib/KeyArgLib.py
@@ -0,0 +1,22 @@
+# -*- coding:UTF-8 -*-
+from inspect import stack as INSPECTstack
+
+# ENSO_metrics package functions:
+from EnsoErrorsWarnings import UnknownKeyArg
+
+
+# ---------------------------------------------------------------------------------------------------------------------#
+#
+# Library to ENSO metrics arguments (arg parser)
+# These functions analyses given arguments and sets some arguments to their default value
+#
+def DefaultArgValues(arg):
+    default = {
+        'detrending': False, 'frequency': None, 'metric_computation': 'difference', 'min_time_steps': None,
+        'normalization': False, 'regridding': False, 'smoothing': False, 'time_bounds': None, 'time_bounds_model': None,
+        'time_bounds_obs': None,
+    }
+    try: default[arg]
+    except: UnknownKeyArg(arg, INSPECTstack())
+    return default[arg]
+# ---------------------------------------------------------------------------------------------------------------------#
