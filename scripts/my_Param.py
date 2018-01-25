f1625b0947927646e61f71c54718c58af819a7a2
lee1043 <lee1043@llnl.gov>
2018-01-25 15:29:57 -0800

    Simplify parameter file to work with multiple OBSs

--- scripts/my_Param.py
+++ scripts/my_Param.py
@@ -1,28 +1,28 @@
 #=================================================
 # Observation
 #-------------------------------------------------
-sstObsPath = '/clim_obs/obs/ocn/mo/tos/UKMETOFFICE-HadISST-v1-1/130122_HadISST_sst.nc'
-tauxObsPath = '/clim_obs/obs/atm/mo/tauu/ERAINT/tauu_ERAINT_198901-200911.nc'
-
-sstNameObs = 'sst' # Units C
-tauxNameObs = 'tauu'
+obspath = {
+    'ERA-Interim': '/clim_obs/obs/atm/mo/VAR/ERAINT/VAR_ERAINT_198901-200911.nc',
+    'HadISST': '/clim_obs/obs/ocn/mo/tos/UKMETOFFICE-HadISST-v1-1/130122_HadISST_sst.nc',
+    'OISST': '/work/lee1043/DATA/OISST/xmls/OISST_tos_mo.xml',
+    'Tropflux': '/work/lee1043/DATA/TropFlux/monthly/xmls/Tropflux_VAR_mo.xml',
+}
 
 #=================================================
 # Models
 #-------------------------------------------------
-modpath = '/work/cmip5/historical/atm/mo/VAR/cmip5.MOD.historical.r1i1p1.mo.atm.Amon.VAR.ver-1.latestX.xml'
+modpath = '/work/lee1043/ESGF/xmls/cmip5/historical/mo/VAR/cmip5.MOD.historical.r1i1p1.mo.VAR.xml'
 
-modnames = ['ACCESS1-0', 'ACCESS1-3', 
-            'BNU-ESM', 
-            'CMCC-CESM', 'CMCC-CM', 'CMCC-CMS', 
-            'CSIRO-Mk3-6-0', 'CanCM4', 
-            'GISS-E2-H-CC', 'GISS-E2-H', 'GISS-E2-R-CC', 'GISS-E2-R', 
+modnames = ['ACCESS1-0', 'ACCESS1-3', 'BCC-CSM1-1', 'BCC-CSM1-1-M', 'BNU-ESM', 
+            'CanCM4', 'CanESM2', 'CCSM4', 'CESM1-BGC', 'CESM1-CAM5', 'CESM1-FASTCHEM', 'CESM1-WACCM', 
+            'CMCC-CESM', 'CMCC-CM', 'CMCC-CMS', 'CNRM-CM5', 'CNRM-CM5-2', 'CSIRO-Mk3-6-0', 
+            'EC-EARTH', 'FGOALS-g2', 'FGOALS-s2', 'FIO-ESM', 'FIO-ESM', 
+            'GFDL-CM2p1', 'GFDL-CM3', 'GFDL-ESM2G', 'GFDL-ESM2M', 
+            'GISS-E2-H', 'GISS-E2-H-CC', 'GISS-E2-R', 'GISS-E2-R-CC', 
             'HadCM3', 'HadGEM2-AO', 'HadGEM2-CC', 'HadGEM2-ES', 
-            'IPSL-CM5A-LR', 
-            'MIROC-ESM-CHEM', 'MIROC-ESM', 'MIROC4h', 'MIROC5', 
-            'MPI-ESM-LR', 'MPI-ESM-MR', 
-            'inmcm4'
-           ]
+            'INMCM4', 'IPSL-CM5A-LR', 'IPSL-CM5A-MR', 'IPSL-CM5B-LR', 
+            'MIROC-ESM', 'MIROC-ESM-CHEM', 'MIROC4h', 'MIROC5', 
+            'MPI-ESM-LR', 'MPI-ESM-MR', 'MPI-ESM-P', 'NorESM1-M', 'NorESM1-ME']
 
 modnames = ['IPSL-CM5A-LR']
 
