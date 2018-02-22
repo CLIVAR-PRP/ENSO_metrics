de183aeda2524a0e4d9594c9f26526623604d79f
lee1043 <lee1043@llnl.gov>
2018-02-21 17:15:28 -0800

    Make the PMP driver work with current version of metrics codes

--- scripts/PMPdriver_EnsoMetrics.py
+++ scripts/PMPdriver_EnsoMetrics.py
@@ -60,12 +60,21 @@ for metric in list_metric:
             if obs not in list_obs:
                 list_obs.append(obs)
 list_obs = sorted(list_obs)
+try:
+    list_obs.remove('ERSSTv5')
+except:
+    pass
+try:
+    list_obs.remove('GPCPv2.3')
+except:
+    pass
 print list_obs
 
 #
 # finding file and variable name in file for each observations dataset
 #
 dict_obs = dict()
+
 for obs in list_obs:
     # @jiwoo: be sure to add your datasets to EnsoCollectionsLib.ReferenceObservations if needed
     dict_var = ReferenceObservations(obs)['variable_name_in_file']
@@ -92,18 +101,13 @@ for obs in list_obs:
             # finding file for 'obs', 'var'
             #
             # @jiwoo: pretty easy as I have all variables in one file
-            #file_name = find_xml_obs(obs, frequency, var0)
-            #file_name = obs+'_'+var0
             file_name = param.obspath[obs].replace('VAR',var0)
-            if not os.path.isfile(file_name): print 'File not available: ', file_name
             # if var_in_file is a list (like for thf) all variables should be read from the same realm
             if isinstance(var_in_file, list):
                 list_files = list()
                 for var1 in var_in_file:
-                    #list_files.append(file_name)
-                    file_name1 = param.obspath[obs].replace('VAR',var1)
-                    if not os.path.isfile(file_name1): print 'File not available: ', file_name1
-                    list_files.append(file_name1)
+                    file_name = param.obspath[obs].replace('VAR',var1)
+                    list_files.append(file_name)
             else:
                 list_files = file_name
             dict_obs[obs][var] = {'path + filename': list_files, 'varname': var_in_file}
@@ -123,137 +127,89 @@ def tree(): return defaultdict(tree)
 enso_stat_dic = tree() # Use tree dictionary to avoid declearing everytime
 
 #######################################################
-#######################################################
 #
 # finding file and variable name in file for each observations dataset
 #
 dict_metric = dict()
 dict_var = CmipVariables()['variable_name_in_file']
 for mod in list_models:
-    dict_mod = {mod: {}}
-    # ------------------------------------------------
-    # @jiwoo: between these dash the program is a bit ad hoc...
-    # it works well for me because I am looking for sst and taux on the ocean grid, and fluxes [lhf, lwr, swr, shf, thf]
-    # on the atmosphere grid
-    # if you want to use atmosphere only, do not use this or create your own way to find the equivalent between the
-    # variable name in the program and the variable name in the file
 
-    print 'jwlee: var loop start for model ', mod
-
-    for var in list_variables:
-        #
-        # finding variable name in file
-        #
-        var_in_file = dict_var[var]['var_name']
-        if isinstance(var_in_file, list):
-            var0 = var_in_file[0]
-        else:
-            var0 = var_in_file
-        #
-        # finding file for 'mod', 'var'
-        #
-        file_name = param.modpath.replace('MOD',mod).replace('VAR',var0)
-        print mod, var0, file_name
-        file = CDMS2open(file_name)
-        if isinstance(var_in_file, list):
-            list_files = list()
-            for var1 in var_in_file:
-                list_files.append(file_name)
-        else:
-            list_files = file_name
-        # ------------------------------------------------
-        dict_mod[mod][var] = {'path + filename': list_files, 'varname': var_in_file}
-
-    print 'jwlee: var loop end'
-
-    # dictionary needed by EnsoMetrics.ComputeMetricsLib.ComputeCollection
-    # @jiwoo the ComputeCollection function it still on development and it does not read the observations requirement
-    # defined in the metric collection, i.e., defCollection(mc_name)['metrics_list']['<metric name>']['obs_name']
-    # so the function does not take a specific obs to compute the metric so for every obs in 'dict_obs' we must include
-    # every variables needed by the metric collection [lhf, lwr, swr, shf, sst, taux, thf] even if its coming from
-    # another dataset
-    dictDatasets = {'model': dict_mod, 'observations': dict_obs}
-    # Computes the metric collection
-    dict_metric[mod] = ComputeCollection(mc_name, dictDatasets)
-    # Prints the metrics values
-    for ii in range (3): print ''
-    print str().ljust(5) + str(mod)
-    list_metric = dict_metric[mod]['metrics'].keys()
-    for metric in list_metric:
-        print str().ljust(10) + str(metric)
-        metric_dict = dict_metric[mod]['metrics'][metric]['metric_values']
-        for ref in metric_dict.keys():
-            print str().ljust(15) + 'metric: ' + str(ref) + ' value = ' + str(metric_dict[ref]['value']) + ', error = '\
-                  + str(metric_dict[ref]['value_error'])
-        raw_dict = dict_metric[mod]['metrics'][metric]['raw_values']['observations']
-        for ref in raw_dict.keys():
-            print str().ljust(15) + 'raw (diag) obs: ' + str(ref) + ' value = ' + str(raw_dict[ref]['value']) +\
-                  ', error = ' + str(raw_dict[ref]['value_error'])
-        raw_dict = dict_metric[mod]['metrics'][metric]['raw_values']['model']
-        print str().ljust(15) + 'raw (diag) model: ' + str(mod) + ' value = ' + str(raw_dict['value']) +\
-                  ', error = ' + str(raw_dict['value_error'])
-
-sys.exit('test')
-#######################################################
-#######################################################
-
-
-for mod in models:
-    print ' ----- ', mod,' ---------------------'
-  
-    if mod == 'obs':
-        sstName = param.sstNameObs
-        tauxName = param.tauxNameObs
-
-        sstFile = param.sstObsPath 
-        tauxFile = param.tauxObsPath
-    else:
-        sstName = param.sstName
-        tauxName = param.tauxName
-
-        sstFile = (param.modpath.replace('MOD', mod)).replace('VAR',sstName) ## Will need land mask out at some point...!
-        tauxFile = (param.modpath.replace('MOD', mod)).replace('VAR',tauxName)
-
-    print sstFile
-    print tauxFile
-  
     try:
+
+        dict_mod = {mod: {}}
+        # ------------------------------------------------
+        # @jiwoo: between these dash the program is a bit ad hoc...
+        # it works well for me because I am looking for sst and taux on the ocean grid, and fluxes [lhf, lwr, swr, shf, thf]
+        # on the atmosphere grid
+        # if you want to use atmosphere only, do not use this or create your own way to find the equivalent between the
+        # variable name in the program and the variable name in the file
+    
+        print 'jwlee: var loop start for model ', mod
+    
+        for var in list_variables:
+            #
+            # finding variable name in file
+            #
+            var_in_file = dict_var[var]['var_name']
+            if isinstance(var_in_file, list):
+                var0 = var_in_file[0]
+            else:
+                var0 = var_in_file
+            #
+            # finding file for 'mod', 'var'
+            #
+            file_name = param.modpath.replace('MOD',mod).replace('VAR',var0)
+            if isinstance(var_in_file, list):
+                list_files = list()
+                for var1 in var_in_file:
+                    file_name = param.modpath.replace('MOD',mod).replace('VAR',var1)
+                    list_files.append(file_name)
+            else:
+                list_files = file_name
+            # ------------------------------------------------
+            dict_mod[mod][var] = {'path + filename': list_files, 'varname': var_in_file}
+    
+        print 'jwlee: var loop end'
+    
+        # dictionary needed by EnsoMetrics.ComputeMetricsLib.ComputeCollection
+        # @jiwoo the ComputeCollection function it still on development and it does not read the observations requirement
+        # defined in the metric collection, i.e., defCollection(mc_name)['metrics_list']['<metric name>']['obs_name']
+        # so the function does not take a specific obs to compute the metric so for every obs in 'dict_obs' we must include
+        # every variables needed by the metric collection [lhf, lwr, swr, shf, sst, taux, thf] even if its coming from
+        # another dataset
+        dictDatasets = {'model': dict_mod, 'observations': dict_obs}
+        # Computes the metric collection
+        dict_metric[mod] = ComputeCollection(mc_name, dictDatasets)
+        # Prints the metrics values
+        for ii in range (3): print ''
+        print str().ljust(5) + str(mod)
+        list_metric = dict_metric[mod]['metrics'].keys()
         for metric in list_metric:
-
-            print metric
-
-            if metric == 'EnsoAmpl':
-                nBox = dict_mc['metrics_list'][metric]['regions']['sst']
-                tmp_dict = EnsoAmpl(sstFile, sstName, nBox)
-                tmp_dict['input_data'] = [sstFile]
-                enso_stat_dic[mod][metric] = tmp_dict
-
-            elif metric == 'EnsoSeasonality':
-                nBox = dict_mc['metrics_list'][metric]['regions']['sst']
-                tmp_dict = EnsoSeasonality(sstFile, sstName, nBox)
-                tmp_dict['input_data'] = [sstFile]
-                enso_stat_dic[mod][metric] = tmp_dict
-
-            elif metric == 'EnsoMu':
-                sstBox = dict_mc['metrics_list'][metric]['regions']['sst']
-                tauxBox = dict_mc['metrics_list'][metric]['regions']['taux']
-                tmp_dict = EnsoMu(sstFile, tauxFile, sstName, tauxName, sstBox, tauxBox)
-                tmp_dict['input_data'] = [sstFile, tauxFile]
-                enso_stat_dic[mod][metric] = tmp_dict
-
-            elif metric == 'EnsoRMSE' and mod != 'obs':
-                nBox = dict_mc['metrics_list'][metric]['regions']['sst']
-                tmp_dict = EnsoRMSE(sstFile, sstName, param.sstObsPath, param.sstNameObs, nBox)
-                tmp_dict['input_data'] = [sstFile]
-                enso_stat_dic[mod][metric] = tmp_dict
-
+            print str().ljust(10) + str(metric)
+            metric_dict = dict_metric[mod]['metrics'][metric]['metric_values']
+            for ref in metric_dict.keys():
+                print str().ljust(15) + 'metric: ' + str(ref) + ' value = ' + str(metric_dict[ref]['value']) + ', error = '\
+                      + str(metric_dict[ref]['value_error'])
+            raw_dict = dict_metric[mod]['metrics'][metric]['raw_values']['observations']
+            for ref in raw_dict.keys():
+                print str().ljust(15) + 'raw (diag) obs: ' + str(ref) + ' value = ' + str(raw_dict[ref]['value']) +\
+                      ', error = ' + str(raw_dict[ref]['value_error'])
+            raw_dict = dict_metric[mod]['metrics'][metric]['raw_values']['model']
+            print str().ljust(15) + 'raw (diag) model: ' + str(mod) + ' value = ' + str(raw_dict['value']) +\
+                      ', error = ' + str(raw_dict['value_error'])
+    
     except Exception as e: 
         print 'failed for ', mod
         print(e)
   
+print 'jwlee: model loop end'
+
 #=================================================
 #  OUTPUT METRICS TO JSON FILE
 #-------------------------------------------------
+enso_stat_dic['obs'] = dict_obs
+enso_stat_dic['model'] = dict_metric
+
 OUT = pcmdi_metrics.io.base.Base(os.path.abspath(param.outpathjson), param.outnamejson)
 
 disclaimer = open(
@@ -263,14 +219,19 @@ disclaimer = open(
         "pmp",
         "disclaimer.txt")).read()
 
+if param.metricsCollection == 'MC1':
+  reference = "The statistics in this file are based on Bellenger, H et al. Clim Dyn (2014) 42:1999-2018. doi:10.1007/s00382-013-1783-z"
+elif param.metricsCollection == 'MC2':
+  reference = "GFDL..."
+
 metrics_dictionary = collections.OrderedDict()
 metrics_dictionary["DISCLAIMER"] = disclaimer
-metrics_dictionary["REFERENCE"] = "The statistics in this file are based on Bellenger, H et al. Clim Dyn (2014) 42:1999-2018. doi:10.1007/s00382-013-1783-z"
+metrics_dictionary["REFERENCE"] = reference
 metrics_dictionary["RESULTS"] = enso_stat_dic
 
 OUT.write(
     metrics_dictionary,
-    json_structure=["model", "metric", "item", "value or description"],
+    json_structure=["type", "data", "metric", "item", "value or description"],
     indent=4,
     separators=(
         ',',
