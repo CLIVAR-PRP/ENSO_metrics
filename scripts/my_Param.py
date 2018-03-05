f589258d2015478bd2fdb9d88eda86ace3d44928
lee1043 <lee1043@llnl.gov>
2018-03-05 11:20:11 -0800

    Remove repeated model in list, which is FIO-ESM

--- scripts/my_Param.py
+++ scripts/my_Param.py
@@ -17,7 +17,7 @@ modpath = '/work/lee1043/ESGF/xmls/cmip5/historical/mo/VAR/cmip5.MOD.historical.
 modnames = ['ACCESS1-0', 'ACCESS1-3', 'BCC-CSM1-1', 'BCC-CSM1-1-M', 'BNU-ESM', 
             'CanCM4', 'CanESM2', 'CCSM4', 'CESM1-BGC', 'CESM1-CAM5', 'CESM1-FASTCHEM', 'CESM1-WACCM', 
             'CMCC-CESM', 'CMCC-CM', 'CMCC-CMS', 'CNRM-CM5', 'CNRM-CM5-2', 'CSIRO-Mk3-6-0', 
-            'EC-EARTH', 'FGOALS-g2', 'FGOALS-s2', 'FIO-ESM', 'FIO-ESM', 
+            'EC-EARTH', 'FGOALS-g2', 'FGOALS-s2', 'FIO-ESM',
             'GFDL-CM2p1', 'GFDL-CM3', 'GFDL-ESM2G', 'GFDL-ESM2M', 
             'GISS-E2-H', 'GISS-E2-H-CC', 'GISS-E2-R', 'GISS-E2-R-CC', 
             'HadCM3', 'HadGEM2-AO', 'HadGEM2-CC', 'HadGEM2-ES', 
@@ -25,17 +25,20 @@ modnames = ['ACCESS1-0', 'ACCESS1-3', 'BCC-CSM1-1', 'BCC-CSM1-1-M', 'BNU-ESM',
             'MIROC-ESM', 'MIROC-ESM-CHEM', 'MIROC4h', 'MIROC5', 
             'MPI-ESM-LR', 'MPI-ESM-MR', 'MPI-ESM-P', 'NorESM1-M', 'NorESM1-ME']
 
-modnames = ['IPSL-CM5A-LR']
+#modnames = ['IPSL-CM5A-LR']
+#modnames = ['BCC-CSM1-1']
 
 #=================================================
 # Output
 #-------------------------------------------------
 outpathdata = '.' # e.g. '/user/directory/output/nc'
 outpathjson = '.' # e.g. '/user/directory/output/json'
-outnamejson = 'test.json'
+#outnamejson = 'test_MC1_all.json'
+outnamejson = 'test_MC2_all.json'
 
 #=================================================
 # Output
 #-------------------------------------------------
 # Metrics Collection
-metricsCollection = 'MC1'
+#metricsCollection = 'MC1'
+metricsCollection = 'MC2'
