b57b30baaa0c99c4d11b9b5a869b21c0866477c1
lee1043 <lee1043@llnl.gov>
2019-07-19 10:46:03 -0700

    pmp param files in separate dir

---
+++ pmp_param/run_pmp.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -a
+
+# Working conda env in Crunchy: pmp_nightly_20180830
+
+ver=`date +"%Y%m%d"`
+
+#mips='cmip5 cmip6'
+mips='cmip5'
+#mips='cmip6'
+
+#MCs='ENSO_perf ENSO_tel ENSO_proc'
+MCs='ENSO_perf ENSO_proc'
+#MCs='ENSO_tel'
+
+for mip in $mips; do
+    for MC in $MCs; do
+        echo $mip $MC
+        python ../scripts/PMPdriver_EnsoMetrics.py -p my_Param_${mip}_${MC}.py >& log.${mip}.${MC}.all.v${ver}.txt &
+        disown
+    done
+done
