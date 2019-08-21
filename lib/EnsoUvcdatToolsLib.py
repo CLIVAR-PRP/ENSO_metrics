7154059fe3a918622e63ec7fad49e04d82224748
yannplanton <johny-jack@hotmail.fr>
2019-08-21 11:54:40 +0200

    more prints for CheckTime function

--- lib/EnsoUvcdatToolsLib.py
+++ lib/EnsoUvcdatToolsLib.py
@@ -994,13 +994,15 @@ def CheckTime(tab1, tab2, frequency='monthly', min_time_steps=None, metric_name=
     # retains only the time-period common to both tab1 and tab2
     tab1_sliced = tab1(time=(stime_adjust, etime_adjust))
     tab2_sliced = tab2(time=(stime_adjust, etime_adjust))
-    tab2_sliced.setAxis(0, tab1_sliced.getTime())
     if debug is True:
         dict_debug = {'shape1': 'tab1.shape = ' + str(tab1_sliced.shape),
                       'shape2': 'tab2.shape = ' + str(tab2_sliced.shape),
                       'time1': 'tab1.time = ' + str(TimeBounds(tab1_sliced)),
-                      'time2': 'tab2.time = ' + str(TimeBounds(tab2_sliced))}
+                      'time2': 'tab1.time = ' + str(tab1_sliced.getTime().asComponentTime()[:]),
+                      'time3': 'tab2.time = ' + str(TimeBounds(tab2_sliced)),
+                      'time4': 'tab2.time = ' + str(tab2_sliced.getTime().asComponentTime()[:])}
         EnsoErrorsWarnings.DebugMode('\033[93m', 'in CheckTime (output)', 20, **dict_debug)
+    tab2_sliced.setAxis(0, tab1_sliced.getTime())
 
     # checks if the remaining time-period fulfills the minimum length criterion
     if min_time_steps is not None:
