8898eaa8a3f76ea0ef1a682b21874abcfcb3de11
lee1043 <lee1043@llnl.gov>
2019-11-06 13:33:01 -0800

    clean up

--- scripts/my_Param_ENSO.py
+++ scripts/my_Param_ENSO.py
@@ -4,9 +4,8 @@ import os
 # =================================================
 # Background Information
 # -------------------------------------------------
-mip = 'cmip5'
-#exp = 'piControl'
-exp = 'historical'
+mip = 'cmip5'  # cmip5, cmip6
+exp = 'historical'  # historical, piControl
 
 #=================================================
 # Miscellaneous
@@ -40,16 +39,7 @@ reference_data_lf_path = {
 modpath = '/work/lee1043/ESGF/xmls/%(mip)/%(exp)/mon/%(variable)/%(mip).%(model).%(exp).%(realization).mon.%(variable).xml'
 modpath_lf = '/work/lee1043/ESGF/xmls/%(mip)/historical/fx/sftlf/%(mip).%(model).historical.r0i0p0.fx.sftlf.xml'
 
-modnames = ['ACCESS1-0', 'ACCESS1-3', 'BCC-CSM1-1', 'BCC-CSM1-1-M', 'BNU-ESM',
-            'CanCM4', 'CanESM2', 'CCSM4', 'CESM1-BGC', 'CESM1-CAM5', 'CESM1-FASTCHEM', 'CESM1-WACCM',
-            'CMCC-CESM', 'CMCC-CM', 'CMCC-CMS', 'CNRM-CM5', 'CNRM-CM5-2', 'CSIRO-Mk3-6-0',
-            'EC-EARTH', 'FGOALS-g2', 'FGOALS-s2', 'FIO-ESM',
-            'GFDL-CM2p1', 'GFDL-CM3', 'GFDL-ESM2G', 'GFDL-ESM2M',
-            'GISS-E2-H', 'GISS-E2-H-CC', 'GISS-E2-R', 'GISS-E2-R-CC',
-            'HadCM3', 'HadGEM2-AO', 'HadGEM2-CC', 'HadGEM2-ES',
-            'INMCM4', 'IPSL-CM5A-LR', 'IPSL-CM5A-MR', 'IPSL-CM5B-LR',
-            'MIROC-ESM', 'MIROC-ESM-CHEM', 'MIROC4h', 'MIROC5',
-            'MPI-ESM-LR', 'MPI-ESM-MR', 'MPI-ESM-P', 'NorESM1-M', 'NorESM1-ME']
+modnames = ['all']
 
 if debug:
     modnames = ['IPSL-CM5A-LR']
@@ -57,7 +47,7 @@ if debug:
 #=================================================
 # Metrics Collection
 #-------------------------------------------------
-metricsCollection = 'ENSO_perf'
+metricsCollection = 'ENSO_perf'  # ENSO_perf, ENSO_tel, ENSO_proc
 
 #=================================================
 # Output
