452c99d18258c827b309f6aacf290edec188b747
lee1043 <lee1043@llnl.gov>
2019-12-18 09:49:10 -0800

    test

--- scripts/run_pmp_parallel.sh
+++ scripts/run_pmp_parallel.sh
@@ -9,30 +9,40 @@ export OMP_NUM_THREADS=1
 
 case_id="v"`date +"%Y%m%d"`
 #case_id="v20191115"
-case_id="v20191121"
+#case_id="v20191121"
 
-mips='cmip5 cmip6'
+#mips='cmip5 cmip6'
 #mips='cmip5'
-#mips='cmip6'
+mips='cmip6'
 
 MCs='ENSO_perf ENSO_tel ENSO_proc'
 #MCs='ENSO_perf'
 #MCs='ENSO_tel ENSO_proc'
 #MCs='ENSO_tel'
-#MCs='ENSO_proc'
+MCs='ENSO_proc'
+#MCs='test_perf test_proc test_tel'
+#MCs='test_proc'
 
 modnames='all'
+#modnames='IPSL-CM5A-LR'
+#modnames='BNU-ESM'
+#modnames='CESM1-BGC'
+#modnames='FGOALS-f3-L'
+modnames='MIROC6'
+modnames='CanESM5 FGOALS-g3'
 
 for mip in $mips; do
     if [ $mip == 'cmip5' ]; then
-        realization='r1i1p1'
+        #realization='r1i1p1'
+        realization='all'
         #modnames="BNU-ESM CESM1-FASTCHEM CMCC-CM FGOALS-g2 HadCM3 HadGEM2-CC IPSL-CM5A-LR IPSL-CM5A-MR MIROC4h MIROC5 MPI-ESM-LR MPI-ESM-MR"
         #modnames="HadGEM2-CC HadGEM2-ES INMCM4 IPSL-CM5A-LR IPSL-CM5A-MR IPSL-CM5B-LR MIROC-ESM MIROC-ESM-CHEM MIROC4h MIROC5 MPI-ESM-LR MPI-ESM-MR MPI-ESM-P MRI-CGCM3 MRI-ESM1 NorESM1-M NorESM1-ME"
-        modnames="FGOALS-s2"
+        #modnames="FGOALS-s2"
     elif [ $mip == 'cmip6' ]; then
         realization='r1i1p1f1'
+        #realization='all'
         #modnames="EC-Earth3 EC-Earth3-Veg"
-        modnames="CNRM-CM6-1 CNRM-ESM2-1 CanESM5 HadGEM3-GC31-LL MIROC-ES2L NorCPM1 NorESM2-LM UKESM1-0-LL"
+        #modnames="CNRM-CM6-1 CNRM-ESM2-1 CanESM5 HadGEM3-GC31-LL MIROC-ES2L NorCPM1 NorESM2-LM UKESM1-0-LL"
     fi
     for MC in $MCs; do
         echo $mip $MC $realization $case_id
