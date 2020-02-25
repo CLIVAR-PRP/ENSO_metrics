dc8d1aa730b7274303e765dd9b5654936ceab470
lee1043 <lee1043@llnl.gov>
2020-02-24 17:16:24 -0800

    update pmpobs path

--- scripts/my_Param_ENSO.py
+++ scripts/my_Param_ENSO.py
@@ -24,7 +24,7 @@ nc_out = True
 # Observation
 #-------------------------------------------------
 reference_data_path = {
-    'ERA-Interim': '/p/user_pub/PCMDIobs/PCMDIobs2.0/atmos/mon/VAR/ERA-INT/gn/v20200117/VAR_mon_ERA-INT_BE_gn_197901-201903.nc',
+    'ERA-Interim': '/p/user_pub/PCMDIobs/PCMDIobs2.0-beta/atmos/mon/VAR/ERA-INT/gn/latest/VAR_mon_ERA-INT_BE_gn_197901-201903.nc',
     'HadISST': '/work/lee1043/DATA/HadISSTv1.1/HadISSTv1.1.xml',
     'OISST': '/work/lee1043/DATA/OISST/xmls/OISST_tos_mo.xml',
     'Tropflux': '/work/lee1043/DATA/TropFlux/monthly/xmls/Tropflux_VAR_mo.xml',
