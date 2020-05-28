0941f0c0300c0dabe155b7456eaf460b3e217bfe
lee1043 <lee1043@llnl.gov>
2020-05-28 09:37:28 -0700

    conflict resolve for pulling

--- lib/version.py
+++ lib/version.py
@@ -1,3 +1,3 @@
 __version__ = 'v0.1'
-__git_tag_describe__ = 'v0.1-412-g90e4eb9'
-__git_sha1__ = '90e4eb961f7df7e3024a73aa5eb5f892930fd0d2'
+__git_tag_describe__ = 'v0.1-415-ga516ce7'
+__git_sha1__ = 'a516ce7f2b887bdb9b782c27614285820e905cd3'
