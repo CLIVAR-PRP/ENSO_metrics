d568d45b11e4823637f531e2e7addfef8a4896d3
lee1043 <lee1043@llnl.gov>
2020-07-17 11:43:38 -0700

    update with code from master

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 import subprocess
 import glob
 
-Version="0.1"
+Version = "1.0-2020"
 
 p = subprocess.Popen(
     ("git",
@@ -38,18 +38,33 @@ print("__git_tag_describe__ = '%s'" % descr, file=f)
 print("__git_sha1__ = '%s'" % commit, file=f)
 f.close()
 
-data_files = (
-              ('share/EnsoMetrics', ['share/EnsoMetrics/basin_generic_1x1deg.nc']),
-             )
+# data_files = (
+#               ('share/EnsoMetrics', ['share/EnsoMetrics/basin_generic_1x1deg.nc']),
+#              )
 
-setup (name="EnsoMetrics",
-       author="Eric Guilyardi",
-       version=Version,
-       description = "Library for ENSO Metrics",
-       url="https://github.com/eguil/ENSO_metrics",
-       packages=['EnsoMetrics'],
-       package_dir={'EnsoMetrics': 'lib'},
-       scripts=glob.glob("scripts/*.py"),
-       data_files=data_files, requires=['numpy']
-       )
-    
+data_files = (
+    (
+        'share/EnsoMetrics',
+        [
+            'share/EnsoMetrics/basin_generic_1x1deg.nc',
+            'share/EnsoMetrics/cmip5_historical_ENSO_perf_v20200427_allModels_allRuns.json',
+            'share/EnsoMetrics/cmip5_historical_ENSO_proc_v20200427_allModels_allRuns.json',
+            'share/EnsoMetrics/cmip5_historical_ENSO_tel_v20200427_allModels_allRuns.json',
+            'share/EnsoMetrics/cmip6_historical_ENSO_perf_v20200427_allModels_allRuns.json',
+            'share/EnsoMetrics/cmip6_historical_ENSO_proc_v20200427_allModels_allRuns.json',
+            'share/EnsoMetrics/cmip6_historical_ENSO_tel_v20200427_allModels_allRuns.json',
+            'share/EnsoMetrics/obs2obs_ENSO_perf_v20200427.json',
+            'share/EnsoMetrics/obs2obs_ENSO_proc_v20200427.json',
+            'share/EnsoMetrics/obs2obs_ENSO_tel_v20200427.json'],
+    ),
+)
+
+setup(name="EnsoMetrics",
+      author="Eric Guilyardi",
+      version=Version,
+      description = "Library for ENSO Metrics",
+      url="https://github.com/CLIVAR-PRP/ENSO_metrics",
+      packages=['EnsoMetrics', 'EnsoPlots'],
+      package_dir={'EnsoMetrics': 'lib', 'EnsoPlots': 'plots'},
+      scripts=glob.glob("scripts/*.py"),
+      data_files=data_files, requires=['numpy'])
