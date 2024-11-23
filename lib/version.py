a203f592c3823767f97a1124f7842e92fe834916
Jiwoo Lee <lee1043@llnl.gov>
2024-11-22 18:56:18 -0800

    update version, fix to work with higher matplotlib version

--- lib/version.py
+++ lib/version.py
@@ -1,3 +1,3 @@
-__version__ = '1.0-2020'
+__version__ = '1.1.3'
 __git_tag_describe__ = '1.0-2020'
-__git_sha1__ = b'8045270291eef94222f6002371775ebb667cbde9'
+__git_sha1__ = b'e8ba34e7dac9630f65e1a959de0e155454cea522'
