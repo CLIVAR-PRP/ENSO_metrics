165ec6e61731c7c3a8b3713b21a7cee68f06f5ea
Jiwoo Lee <lee1043@llnl.gov>
2024-11-22 19:06:14 -0800

    clean up

--- lib/version.py
+++ lib/version.py
@@ -1,3 +1,3 @@
 __version__ = '1.1.3'
-__git_tag_describe__ = '1.0-2020'
-__git_sha1__ = b'e8ba34e7dac9630f65e1a959de0e155454cea522'
+__git_tag_describe__ = '1.1.3'
+__git_sha1__ = b'd877b5c4ce7cbc41bddf43120ea50db604b81142'
