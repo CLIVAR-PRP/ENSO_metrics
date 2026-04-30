76e8f315ca8227557e1cf676878033e55e5f2e91
shixuan zhang <shixuan.zhang@pnnl.gov>
2026-04-30 12:54:52 -0700

    fix to not update version.py during each compiling

--- lib/version.py
+++ lib/version.py
@@ -1,3 +1,1 @@
 __version__ = '1.2.0'
-__git_tag_describe__ = '1.2.0'
-__git_sha1__ = b'36a88d87680ff87716cf9ea67338721a4240b7bb'
--- setup.py
+++ setup.py
@@ -1,41 +1,12 @@
 from setuptools import setup
-import subprocess
 import glob
+import re
 
-Version = "1.2.0"
-
-p = subprocess.Popen(
-    ("git",
-     "describe",
-     "--tags"),
-    stdin=subprocess.PIPE,
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE)
-try:
-    descr = p.stdout.readlines()[0].strip()
-    Version = "-".join(descr.split("-")[:-2])
-    if Version == "":
-        Version = descr
-except:
-    descr = Version
-
-p = subprocess.Popen(
-    ("git",
-     "log",
-     "-n1",
-     "--pretty=short"),
-    stdin=subprocess.PIPE,
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE)
-try:
-    commit = str(p.stdout.readlines()[0].split()[1])
-except:
-    commit = "unknown"
-f = open("lib/version.py", "w")
-print("__version__ = '%s'" % Version, file=f)
-print("__git_tag_describe__ = '%s'" % descr, file=f)
-print("__git_sha1__ = %s" % commit, file=f)
-f.close()
+# Read the version from lib/version.py — that file is the single source of
+# truth and is tracked by git.  setup.py no longer writes it.
+with open("lib/version.py") as _vf:
+    _src = _vf.read()
+Version = re.search(r"__version__\s*=\s*'([^']+)'", _src).group(1)
 
 # data_files = (
 #               ('share/EnsoMetrics', ['share/EnsoMetrics/basin_generic_1x1deg.nc']),
